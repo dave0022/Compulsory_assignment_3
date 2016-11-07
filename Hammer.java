1fcad2e7b7a12d393ba1c7274a0fffb34f64fe7b
kasperfage <kasperfage@hotmail.com>
2016-11-07 11:44:42 +0100

    added and removed classes
    added the game and hammer class, removed hole class

--- Hammer.java
+++ Hammer.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Hole here.
+ * Write a description of class hammer here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Hole extends Actor
+public class Hammer extends Actor
 {
     /**
-     * Act - do whatever the Hole wants to do. This method is called whenever
+     * Act - do whatever the hammer wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
